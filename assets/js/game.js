0d0cd6286f2f201670b684ed425276c611c24ee5
Ritchie <r.ortiz54@yahoo.com>
2021-02-19 02:38:53 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,10 @@
-var playerName = window.prompt("What is your robot's names?");
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
@@ -8,11 +14,22 @@ console.log(playerName);
 console.log(playerHealth);
 console.log("You can put regular sentences in these.");
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]; 
 var enemyHealth = 50;
 var enemyAttack = 12;
+console.log(enemyNames.length);
+enemyNames[0];
+enemyNames[1];
+enemyNames[2];
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + "  index");
+}
 
-var fight = function() {
+var fight = function(enemyName) {
+  ... // fight function statements
+}
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -67,4 +84,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 }
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
